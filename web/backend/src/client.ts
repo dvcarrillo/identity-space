c873cea6e647fff46eb68070c4200e49691cf669
Christoph Johns <christophjohns@gmail.com>
2020-11-28 16:45:51 +0100

    Add basic socket.io NFC communication

--- web/backend/src/client.ts
+++ web/backend/src/client.ts
@@ -5,15 +5,15 @@ const socket = manager.socket("/");
 
 socket.on("connect", () => {
     console.log(`connect ${socket.id}`);
+    const nfcID = "32:F4:56:9B";
+    console.log(`sending NFC ID: ${nfcID}`);
+    socket.emit("nfcID", nfcID, (response) => {
+        if (response.status == "ok") {
+            console.log(`acknowledged NFC ID: ${response.acknowledgedNFCID}`);
+        }
+    });
 });
 
 socket.on("disconnect", () => {
     console.log(`disconnect`);
 });
-
-setInterval(() => {
-    const start = Date.now();
-    socket.emit("ping", () => {
-        console.log(`pong (latency: ${Date.now() - start} ms)`);
-    });
-}, 1000);
--- web/backend/src/server.ts
+++ web/backend/src/server.ts
@@ -5,9 +5,13 @@ const io = new Server(8080);
 io.on("connect", (socket: Socket) => {
     console.log(`connect ${socket.id}`);
 
-    socket.on("ping", (cb) => {
-        console.log("ping");
-        cb();
+    socket.on("nfcID", (nfcID, callback) => {
+        console.log(`received NFC ID: ${nfcID}`);
+        console.log("acknowledging NFC ID");
+        callback({
+            status: "ok",
+            acknowledgedNFCID: nfcID,
+        });
     });
 
     socket.on("disconnect", () => {
