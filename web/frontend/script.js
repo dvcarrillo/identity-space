52a54cb9116a65c4cdbff67ee926f7bfc903a69d
Christoph Johns <christophjohns@gmail.com>
2020-11-28 22:56:10 +0100

    Add static frontend client to display currently connected NFC IDs

---
+++ web/frontend/script.js
@@ -0,0 +1,22 @@
+const socket = io("ws://localhost:8080");
+const ulNFCIDs = document.querySelector("ul#idList");
+const template = document.querySelector("template#nfcID");
+
+socket.on("connect", () => {
+    console.log(`connect ${socket.id}`);
+});
+
+socket.on("disconnect", () => {
+    console.log(`disconnect`);
+});
+
+socket.on("nfcIDs", (nfcIDs) => {
+    console.log(`received NFC IDs: ${nfcIDs}`);
+    ulNFCIDs.innerHTML = "";
+    nfcIDs.map(nfcID => {
+        const clone = template.content.cloneNode(true);
+        const li = clone.querySelector("li");
+        li.textContent = nfcID;
+        ulNFCIDs.appendChild(clone);
+    });
+})
