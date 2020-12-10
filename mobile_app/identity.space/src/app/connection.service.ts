a65b0473fa29fe3510ed1b730e12e7e774fc7f46
David Vargas <dv.carrillo@yahoo.com>
2020-12-10 22:19:18 +0100

    Added socket connection test on Settings tab

--- mobile_app/identity.space/src/app/connection.service.ts
+++ mobile_app/identity.space/src/app/connection.service.ts
@@ -1,11 +1,48 @@
-import { Injectable } from '@angular/core';
+import { Injectable, OnInit } from '@angular/core';
+import { io } from 'socket.io-client';
 
 @Injectable({
   providedIn: 'root'
 })
-export class ConnectionService {
-  serverAddress : string = '192.168.1.1';
-  serverPort : string = '8080';
+export class ConnectionService implements OnInit {
+  private socket: any;
+  public serverAddress = '192.168.1.1';
+  public serverPort = '8080';
+  public data: any;
 
   constructor() { }
+
+  public ngOnInit() {
+    this.socket.on('notification', data => {
+      this.data = data;
+    });
+  }
+
+  public connect() {
+    this.socket = io(`http://${this.serverAddress}:${this.serverPort}`);
+    this.socket.on('connect', () => { console.log('Connected to socket: ', this.socket) });
+    // socket.on('event', function (data) { });
+    this.socket.on('disconnect', () => { console.log('Disconnected from socket') });
+  }
+
+  public sendTestString(testString: string) {
+    this.socket.emit('appTest', testString, (data) => {
+      console.log('Received from socket - appTest:', data);
+    });
+  }
+
+  public sendNfcString(nfcString: string) {
+    this.socket.emit('nfcID', nfcString, (data) => {
+      console.log('Received from socket - nfcID:', data);
+    });
+  }
+
+  public isSocketConnected(): boolean {
+    if ((this.socket != undefined) && (this.socket.connected == true)) {
+      return true;
+    }
+    else {
+      return false;
+    }
+  }
 }
--- mobile_app/identity.space/src/app/tab2/tab2.page.ts
+++ mobile_app/identity.space/src/app/tab2/tab2.page.ts
@@ -8,15 +8,16 @@ import { Component } from '@angular/core';
 })
 export class Tab2Page {
 
-  private serverAddress : string;
-  private serverPort : string;
+  private serverAddress: string;
+  private serverPort: string;
 
   // For testing web app connection
-  private testSendingString : string = "";
-  private testSent : boolean = false;
-  private testResponse : String = "Loading..."
+  private testSendingString: string = '';
+  private testSent: boolean = false;
+  private testResponse: String = 'Loading...';
+  private testType: string = 'check';
 
-  constructor(private connectionService : ConnectionService) {}
+  constructor(private connectionService: ConnectionService) { }
 
   ionViewWillEnter() {
     this.serverAddress = this.connectionService.serverAddress;
@@ -26,11 +27,34 @@ export class Tab2Page {
   ionViewWillLeave() {
     this.connectionService.serverAddress = this.serverAddress;
     this.connectionService.serverPort = this.serverPort;
+    this.testSent = false;
   }
 
   sendButtonClicked() {
-    this.testResponse = `Sending "${this.testSendingString}" to ${this.serverAddress}:${this.serverPort}...`;
+    this.connectionService.serverAddress = this.serverAddress;
+    this.connectionService.serverPort = this.serverPort;
+
+    this.testResponse = `Sending "${this.testSendingString}" to http://${this.serverAddress}:${this.serverPort}...`;
+    this.connectionService.connect();
     this.testSent = true;
+
+    setTimeout(() => {
+      if (this.connectionService.isSocketConnected()) {
+        this.testResponse = `Connected to socket on http://${this.serverAddress}:${this.serverPort}`;
+
+        if (this.testType === 'nfc') {
+          this.connectionService.sendNfcString(this.testSendingString);
+          this.testResponse = 'The NFC ID has been received by the socket.';
+        }
+        else if (this.testType === 'check') {
+          this.connectionService.sendTestString(this.testSendingString);
+          this.testResponse = 'Response from socket received. Web server is on.';
+        }
+      }
+      else {
+        this.testResponse = 'Socket is disconnected.';
+      }
+    }, 2000);
   }
 
 }
